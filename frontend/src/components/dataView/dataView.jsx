faeba868aa5cfa71aecab0f2e9d2648996537f3c
William Gislason <8203830+gislawill@users.noreply.github.com>
2024-06-07 18:51:51 -0700

    more mobile styles

--- frontend/src/components/dataView/dataView.jsx
+++ frontend/src/components/dataView/dataView.jsx
@@ -1,15 +1,15 @@
 import { listToString } from "../../utilities/text-formatting";
 import "./dataView.css";
 
-export function DetailDemo({ url }) {
+export function DetailDemo({ url, forMobile = false }) {
   if (!url.includes("youtu.be"))
     return (
-      <div className="video site">
+      <div className={forMobile ? "video site mobile" : "video site"}>
         <iframe width="100%" height="100%" src={url}></iframe>
       </div>
     );
   return (
-    <div className="video">
+    <div className={forMobile ? "video mobile" : "video"}>
       <div>
         <a href={url} target="_blank">
           Link to Site
@@ -32,63 +32,57 @@ function Block({ className, title, content }) {
 export default function DataView({ data, classes = [] }) {
   return (
     <>
-    <img src={data.Thumbnail} alt={data.Title} className="thumbnail" />
-    <div className={"data-view " + classes.join(" ")}>
-      <div className="title">{data.Title}</div>
-      <Block
-        className="type"
-        title=""
-        content={
-          <div className="content">
-            <div>{data.Tags}</div>
-          </div>
-        }
-      />
-      <div className="subtitle">{data["Short description"]}</div>
-      <Block
-        className="authors"
-        title="Authors"
-        content={
-          <div className="content">
-            {listToString(data.Authors)}
-          </div>
-        }
-      />
-      <Block
-        className="institutions"
-        title="Institutions"
-        content={
-          <div className="content">
-            {listToString(data.Institutions)}
-          </div>
-        }
-      />
-      {data.Publication && (
+      <img src={data.Thumbnail} alt={data.Title} className="thumbnail" />
+      <div className={"data-view " + classes.join(" ")}>
+        <div className="title">{data.Title}</div>
         <Block
-          className="publication"
-          title="Publication"
+          className="type"
+          title=""
           content={
-            <div>
-              <a
-                className="button-link"
-                href={data.Publication}
-                target="_blank"
-              >
-                {data.Publication}
-              </a>
+            <div className="content">
+              <div>{data.Tags}</div>
             </div>
           }
         />
-      )}
-      {data.URL && (
-        <div className="button-link">
-        <a  href={data.URL} target="_blank">
-          View site
-          </a>
-        </div>
-      )}
-    </div>
+        <div className="subtitle">{data["Short description"]}</div>
+        <Block
+          className="authors"
+          title="Authors"
+          content={<div className="content">{listToString(data.Authors)}</div>}
+        />
+        <Block
+          className="institutions"
+          title="Institutions"
+          content={
+            <div className="content">{listToString(data.Institutions)}</div>
+          }
+        />
+        {data.Publication && (
+          <Block
+            className="publication"
+            title="Publication"
+            content={
+              <div>
+                <a
+                  className="button-link"
+                  href={data.Publication}
+                  target="_blank"
+                >
+                  {data.Publication}
+                </a>
+              </div>
+            }
+          />
+        )}
+        {data.URL && (
+          <div className="button-link">
+            <a href={data.URL} target="_blank">
+              View site
+            </a>
+          </div>
+        )}
+      </div>
+      <DetailDemo url={data.URL} forMobile={true} />
     </>
-    
   );
 }
